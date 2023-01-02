5cc89c07889d41b1b067728016d28ea9118dc156
volki9000 <volkitrocity@gmx.de>
2023-01-02 17:50:41 +0100

    [refac] Use match case instead of if tree

--- README.md
+++ README.md
@@ -1,11 +1,63 @@
-# Evenpog
+# Diopser
 
-Based on https://github.com/robbert-vdh/nih-plug
+You were expecting Disperser[¹](#disperser), but it was me, Diopser!
 
-## Building
+Diopser lets you rotate the phase of a signal around a specific frequency
+without affecting its spectral content. This effect can be used to emphasize
+transients and other parts of a sound that in a way that isn't possible with
+regular equalizers or dynamics processors, especially when applied to low
+pitched or wide band sounds. More extreme settings will make everything sound
+like a cartoon laser beam, or a psytrance kickdrum.
 
-After installing [Rust](https://rustup.rs/), you can compile Evenpog as follows:
+![Screenshot](https://i.imgur.com/QLtHtQL.png)
+
+This is a port of https://github.com/robbert-vdh/diopser with more features and
+much better performance.
+
+<sup id="disperser">
+  *Disperser is a trademark of Kilohearts AB. Diopser is in no way related to
+  Disperser or Kilohearts AB.
+</sup>
+
+## Tips
+
+- Alt+click on the spectrum analyzer to enter to enter a frequency value in
+  Hertz or musical notes.
+- Hold down Alt/Option while dragging the filter frequency around to snap to
+  whole notes.
+- The safe mode is enabled by default. This limits the frequency range and the
+  number of filter stages. Simply disable the safe mode if you want to crank
+  everything up to 11. With safe mode disabled you may find that going down to
+  the bottom of the frequency range introduces some loud low frequency
+  resonances, especially when combined with a lot of filter stages. In that case
+  you may want to use a peak limiter after this plugin until you understand how
+  it reacts to different changes. Or maybe you'll want to check out [Safety
+  Limiter](../safety_limiter), which is made for this exact purpose.
+- Turn down the automation precision to reduce the DSP load hit of changing the
+  filter frequency and resonance at the cost of introducing varying amounts of
+  aliasing and zipper noises.
+- The aforementioned artifacts introduced by setting a low automation precision
+  can actually be useful for sound design purposes.
+- Change the number of filter stages to immediately reset the filters and stop
+  ringing.
+
+## Download
+
+You can download the development binaries for Linux, Windows and macOS from the
+[automated
+builds](https://github.com/robbert-vdh/nih-plug/actions/workflows/build.yml?query=branch%3Amaster)
+page. Or if you're not signed in on GitHub, then you can also find the latest nightly
+build [here](https://nightly.link/robbert-vdh/nih-plug/workflows/build/master).
+
+On macOS you may need to [disable
+Gatekeeper](https://disable-gatekeeper.github.io/) as Apple has recently made it
+more difficult to run unsigned code on macOS.
+
+### Building
+
+After installing [Rust](https://rustup.rs/) with the nightly toolchain (because
+of the use of SIMD), you can compile Diopser as follows:
 
 ```shell
-cargo +nightly xtask bundle evenpog --release
+cargo +nightly xtask bundle diopser --release
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
-use std::sync::Arc;
+use dwt;
 use nih_plug::prelude::*;
+use std::sync::Arc;
+
+use dwt::{wavelet, Operation, Transform};
 
 /// The maximum number of samples to iterate over at a time.
 const MAX_BLOCK_SIZE: usize = 64;
@@ -11,7 +14,10 @@ fn highpass20hz(sample: f32) -> f32 {
     static GAIN: f32 = CUTOFF_FREQUENCY_HZ / (2.0f32 * std::f32::consts::PI * 44100.0f32);
     let state = STATE.load(std::sync::atomic::Ordering::Acquire);
     let retval = sample - state;
-    STATE.store(state + (GAIN * retval), std::sync::atomic::Ordering::Release);
+    STATE.store(
+        state + (GAIN * retval),
+        std::sync::atomic::Ordering::Release,
+    );
     retval
 }
 
@@ -35,26 +41,26 @@ pub struct EvenPogParams {
     #[id = "slurrate"]
     pub slur_multiplier: FloatParam,
 
-    #[id = "shavevapor0x"]
-    pub shavevapor_x0: FloatParam,
-    #[id = "shavevapor1x"]
-    pub shavevapor_x1: FloatParam,
-    #[id = "shavevapor2x"]
-    pub shavevapor_x2: FloatParam,
-    #[id = "shavevapor3x"]
-    pub shavevapor_x3: FloatParam,
-    #[id = "shavevapor4x"]
-    pub shavevapor_x4: FloatParam,
-    #[id = "shavevapor5x"]
-    pub shavevapor_x5: FloatParam,
-    #[id = "shavevapor6x"]
-    pub shavevapor_x6: FloatParam,
-    #[id = "shavevapor7x"]
-    pub shavevapor_x7: FloatParam,
-    #[id = "shavevapor8x"]
-    pub shavevapor_x8: FloatParam,
-    #[id = "shavevapor9x"]
-    pub shavevapor_x9: FloatParam,
+    #[id = "shavevapor0y"]
+    pub shavevapor_y0: FloatParam,
+    #[id = "shavevapor1y"]
+    pub shavevapor_y1: FloatParam,
+    #[id = "shavevapor2y"]
+    pub shavevapor_y2: FloatParam,
+    #[id = "shavevapor3y"]
+    pub shavevapor_y3: FloatParam,
+    #[id = "shavevapor4y"]
+    pub shavevapor_y4: FloatParam,
+    #[id = "shavevapor5y"]
+    pub shavevapor_y5: FloatParam,
+    #[id = "shavevapor6y"]
+    pub shavevapor_y6: FloatParam,
+    #[id = "shavevapor7y"]
+    pub shavevapor_y7: FloatParam,
+    #[id = "shavevapor8y"]
+    pub shavevapor_y8: FloatParam,
+    #[id = "shavevapor9y"]
+    pub shavevapor_y9: FloatParam,
 
     #[id = "gain"]
     pub gain: FloatParam,
@@ -67,89 +73,168 @@ impl EvenPogParams {
                 .with_value_to_string(formatters::v2s_bool_bypass())
                 .with_string_to_value(formatters::s2v_bool_bypass())
                 .make_bypass(),
-            mix_dry: FloatParam::new("DryMix", 0.5f32, FloatRange::Linear { min: 0.0f32, max: 1.0f32 }),
-            mix_slur: FloatParam::new("SlurMix", 0.5f32, FloatRange::Linear { min: 0.0f32, max: 1.0f32 }),
-            mix_hfj: FloatParam::new("HonkForJesusMix", 0.5f32, FloatRange::Linear { min: 0.0f32, max: 1.0f32 }),
-            buffer_length: IntParam::new("BufferLength", 512, IntRange::Linear { min: 16, max: BUFFER_SIZE as i32 }),
-            buffer_acceleration: IntParam::new("HonkForJesusAcceleration", 10, IntRange::Linear {
-                min: -1000,
-                max: 1000
-            }),
-            slur_multiplier: FloatParam::new("SlurMultiplier", 1.2f32, FloatRange::Linear {
-                min: 0.1f32,
-                max: 10.0f32
-            }),
+            mix_dry: FloatParam::new(
+                "DryMix",
+                0.5f32,
+                FloatRange::Linear {
+                    min: 0.0f32,
+                    max: 1.0f32,
+                },
+            ),
+            mix_slur: FloatParam::new(
+                "SlurMix",
+                0.5f32,
+                FloatRange::Linear {
+                    min: 0.0f32,
+                    max: 1.0f32,
+                },
+            ),
+            mix_hfj: FloatParam::new(
+                "HonkForJesusMix",
+                0.5f32,
+                FloatRange::Linear {
+                    min: 0.0f32,
+                    max: 1.0f32,
+                },
+            ),
+            buffer_length: IntParam::new(
+                "BufferLength",
+                512,
+                IntRange::Linear {
+                    min: 16,
+                    max: BUFFER_SIZE as i32,
+                },
+            ),
+            buffer_acceleration: IntParam::new(
+                "HonkForJesusAcceleration",
+                10,
+                IntRange::Linear {
+                    min: -1000,
+                    max: 1000,
+                },
+            ),
+            slur_multiplier: FloatParam::new(
+                "SlurMultiplier",
+                1.2f32,
+                FloatRange::Linear {
+                    min: 0.1f32,
+                    max: 10.0f32,
+                },
+            ),
 
-            shavevapor_x0: FloatParam::new("ShaveVapor_0x", -1.0f32, FloatRange::Linear {
-                min: -1.0f32,
-                max: 1.0f32
-            }),
-            shavevapor_x1: FloatParam::new("ShaveVapor_1x", -0.8f32, FloatRange::Linear {
-                min: -1.0f32,
-                max: 1.0f32
-            }),
-            shavevapor_x2: FloatParam::new("ShaveVapor_2x", -0.6f32, FloatRange::Linear {
-                min: -1.0f32,
-                max: 1.0f32
-            }),
-            shavevapor_x3: FloatParam::new("ShaveVapor_3x", -0.4f32, FloatRange::Linear {
-                min: -1.0f32,
-                max: 1.0f32
-            }),
-            shavevapor_x4: FloatParam::new("ShaveVapor_4x", -0.2f32, FloatRange::Linear {
-                min: -1.0f32,
-                max: 1.0f32
-            }),
-            shavevapor_x5: FloatParam::new("ShaveVapor_5x", 0.2f32, FloatRange::Linear {
-                min: -1.0f32,
-                max: 1.0f32
-            }),
-            shavevapor_x6: FloatParam::new("ShaveVapor_6x", 0.4f32, FloatRange::Linear {
-                min: -1.0f32,
-                max: 1.0f32
-            }),
-            shavevapor_x7: FloatParam::new("ShaveVapor_7x", 0.6f32, FloatRange::Linear {
-                min: -1.0f32,
-                max: 1.0f32
-            }),
-            shavevapor_x8: FloatParam::new("ShaveVapor_8x", 0.8f32, FloatRange::Linear {
-                min: -1.0f32,
-                max: 1.0f32
-            }),
-            shavevapor_x9: FloatParam::new("ShaveVapor_9x", 1.0f32, FloatRange::Linear {
-                min: -1.0f32,
-                max: 1.0f32
-            }),
+            shavevapor_y0: FloatParam::new(
+                "ShaveVapor_0y",
+                -1.0f32,
+                FloatRange::Linear {
+                    min: -1.0f32,
+                    max: 1.0f32,
+                },
+            ),
+            shavevapor_y1: FloatParam::new(
+                "ShaveVapor_1y",
+                -0.8f32,
+                FloatRange::Linear {
+                    min: -1.0f32,
+                    max: 1.0f32,
+                },
+            ),
+            shavevapor_y2: FloatParam::new(
+                "ShaveVapor_2y",
+                -0.6f32,
+                FloatRange::Linear {
+                    min: -1.0f32,
+                    max: 1.0f32,
+                },
+            ),
+            shavevapor_y3: FloatParam::new(
+                "ShaveVapor_3y",
+                -0.4f32,
+                FloatRange::Linear {
+                    min: -1.0f32,
+                    max: 1.0f32,
+                },
+            ),
+            shavevapor_y4: FloatParam::new(
+                "ShaveVapor_4y",
+                -0.2f32,
+                FloatRange::Linear {
+                    min: -1.0f32,
+                    max: 1.0f32,
+                },
+            ),
+            shavevapor_y5: FloatParam::new(
+                "ShaveVapor_5y",
+                0.2f32,
+                FloatRange::Linear {
+                    min: -1.0f32,
+                    max: 1.0f32,
+                },
+            ),
+            shavevapor_y6: FloatParam::new(
+                "ShaveVapor_6y",
+                0.4f32,
+                FloatRange::Linear {
+                    min: -1.0f32,
+                    max: 1.0f32,
+                },
+            ),
+            shavevapor_y7: FloatParam::new(
+                "ShaveVapor_7y",
+                0.6f32,
+                FloatRange::Linear {
+                    min: -1.0f32,
+                    max: 1.0f32,
+                },
+            ),
+            shavevapor_y8: FloatParam::new(
+                "ShaveVapor_8y",
+                0.8f32,
+                FloatRange::Linear {
+                    min: -1.0f32,
+                    max: 1.0f32,
+                },
+            ),
+            shavevapor_y9: FloatParam::new(
+                "ShaveVapor_9y",
+                1.0f32,
+                FloatRange::Linear {
+                    min: -1.0f32,
+                    max: 1.0f32,
+                },
+            ),
 
-            gain: FloatParam::new("Gain", 1.0f32, FloatRange::Linear {
-                min: 0.1f32,
-                max: 24.0f32
-            })
+            gain: FloatParam::new(
+                "Gain",
+                1.0f32,
+                FloatRange::Linear {
+                    min: 0.1f32,
+                    max: 24.0f32,
+                },
+            ),
         }
     }
 }
 
 pub struct EvenPog {
-    params : Arc<EvenPogParams>,
+    params: Arc<EvenPogParams>,
     delay_buffer: [f32; BUFFER_SIZE],
     buffer_write_position: usize,
     buffer_read_position_slur: usize,
     buffer_read_position_slur_exact: f32,
     buffer_read_position_hfj: usize,
-    buffer_rate_smps: usize
+    buffer_rate_smps: usize,
 }
 
 impl Default for EvenPog {
     fn default() -> Self {
         Self {
-            params: Arc::new(EvenPogParams::new(
-            )),
-            delay_buffer: [0.0f32;BUFFER_SIZE],
+            params: Arc::new(EvenPogParams::new()),
+            delay_buffer: [0.0f32; BUFFER_SIZE],
             buffer_write_position: 0,
             buffer_read_position_slur: 0,
             buffer_read_position_slur_exact: 0.1f32,
             buffer_read_position_hfj: 0,
-            buffer_rate_smps: 22050
+            buffer_rate_smps: 22050,
         }
     }
 }
@@ -183,25 +268,48 @@ impl Plugin for EvenPog {
         if !self.params.bypass.value() {
             for (_, mut block) in buffer.iter_blocks(MAX_BLOCK_SIZE) {
                 for channel_samples in block.iter_samples() {
-                    for sample in channel_samples.into_iter()
-                    {
-                        self.delay_buffer[self.buffer_write_position % self.params.buffer_length.value() as usize] = *sample;
+                    for sample in channel_samples.into_iter() {
+                        self.delay_buffer[self.buffer_write_position
+                            % self.params.buffer_length.value() as usize] = *sample;
                         self.buffer_write_position = self.buffer_write_position + 1;
 
-                        let s = self.delay_buffer[self.buffer_read_position_slur] * self.params.mix_slur.value() + self.delay_buffer[self.buffer_read_position_hfj] * self.params.mix_hfj.value();
+                        let dry = *sample * self.params.mix_dry.value();
+                        let s = self.delay_buffer[self.buffer_read_position_slur]
+                            * self.params.mix_slur.value()
+                            + self.delay_buffer[self.buffer_read_position_hfj]
+                                * self.params.mix_hfj.value();
                         let s2 = s * s;
                         let s3 = s2 * s;
 
-                        *sample = s * self.params.mix_dry.value() + self.params.gain.value() * self.waveshape(highpass20hz(s - (s2/8.0f32) - (s3/16.0f32) + 0.125f32));
-                        self.buffer_read_position_slur_exact = self.buffer_read_position_slur as f32 * self.params.slur_multiplier.value();
+                        *sample = self.params.gain.value()
+                            * self.waveshape(
+                                dry + highpass20hz(s - (s2 / 8.0f32) - (s3 / 16.0f32) + 0.125f32),
+                            );
+                        self.buffer_read_position_slur_exact = self.buffer_read_position_slur
+                            as f32
+                            * self.params.slur_multiplier.value();
                         if self.buffer_read_position_slur_exact < 0.1f32 {
                             self.buffer_read_position_slur_exact = 1.0f32;
                         }
-                        self.buffer_read_position_slur = self.buffer_read_position_slur_exact as usize % self.params.buffer_length.value() as usize;
-                        self.buffer_read_position_hfj = (self.params.buffer_length.value() as usize + self.buffer_read_position_hfj - self.buffer_rate_smps)
+                        self.buffer_read_position_slur = self.buffer_read_position_slur_exact
+                            as usize
+                            % self.params.buffer_length.value() as usize;
+                        self.buffer_read_position_hfj = (self.params.buffer_length.value()
+                            as usize
+                            + self.buffer_read_position_hfj
+                            - self.buffer_rate_smps)
                             % self.params.buffer_length.value() as usize;
 
-                        self.buffer_rate_smps = ((self.buffer_rate_smps as i32 + self.params.buffer_acceleration.value()).abs() % 22050) as usize; 
+                        self.buffer_rate_smps = ((self.buffer_rate_smps as i32
+                            + self.params.buffer_acceleration.value())
+                        .abs()
+                            % 22050) as usize;
+
+                        let mut data = vec![42.0; 64];
+                        let operation = Operation::Forward;
+                        let wavelet = wavelet::Haar::new();
+                        let level = (64 as f64).log2() as usize;
+                        data.transform(operation, &wavelet, level)
                     }
                 }
             }
@@ -211,59 +319,79 @@ impl Plugin for EvenPog {
 }
 
 impl EvenPog {
-    fn waveshape(
-        &mut self,
-        sample: f32,
-    ) -> f32 {
-        if sample > 0.1f32 {
-            if sample > 0.6f32 {
-                if sample > 0.8f32 {
-                    return sample * self.params.shavevapor_x9.value();
-                }
-                else {
-                    return sample * self.params.shavevapor_x8.value();
-                }
-            }
-            else if sample > 0.4f32 {
-                return sample * self.params.shavevapor_x7.value();
-            }
-            else {
-                if sample > -0.2f32 {
-                    return sample * self.params.shavevapor_x5.value();
-                }
-                else {
-                    return sample * self.params.shavevapor_x6.value();
-                }
-            }
+    fn waveshape(&mut self, sample: f32) -> f32 {
+        struct ShaperSet {
+            multiplier0: f32,
+            multiplier1: f32,
+            sample_weighted: f32,
         }
-        else if sample < -0.1f32 {
-            if sample < -0.6f32 {
-                if sample < -0.8f32 {
-                    return sample * self.params.shavevapor_x0.value();
-                }
-                else {
-                    return sample * self.params.shavevapor_x1.value();
-                }
-            }
-            else if sample < -0.4f32 {
-                if sample < -0.2f32 {
-                    return sample * self.params.shavevapor_x4.value();
-                }
-                else {
-                    return sample * self.params.shavevapor_x3.value();
-                }
-            }
-            else {
-                return sample * self.params.shavevapor_x2.value();
-            }
-        }
-        return sample;
+
+        let s = match sample {
+            d if d < -0.8f32 => ShaperSet {
+                multiplier0: self.params.shavevapor_y0.value(),
+                multiplier1: self.params.shavevapor_y1.value(),
+                sample_weighted: (d + 0.8f32 * 5.0f32),
+            },
+            d if d < -0.8f32 && d > -0.6f32 => ShaperSet {
+                multiplier0: self.params.shavevapor_y1.value(),
+                multiplier1: self.params.shavevapor_y2.value(),
+                sample_weighted: (d + 0.6f32 * 5.0f32),
+            },
+            d if d < -0.6f32 && d > -0.4f32 => ShaperSet {
+                multiplier0: self.params.shavevapor_y2.value(),
+                multiplier1: self.params.shavevapor_y3.value(),
+                sample_weighted: (d + 0.4f32 * 5.0f32),
+            },
+            d if d < -0.4f32 && d > -0.2f32 => ShaperSet {
+                multiplier0: self.params.shavevapor_y3.value(),
+                multiplier1: self.params.shavevapor_y4.value(),
+                sample_weighted: (d + 0.2f32 * 5.0f32),
+            },
+            d if d < -0.2f32 && d > -0.1f32 => ShaperSet {
+                multiplier0: self.params.shavevapor_y4.value(),
+                multiplier1: 0.0f32,
+                sample_weighted: (d * 5.0f32),
+            },
+            d if d > 0.1f32 && d < 0.2f32 => ShaperSet {
+                multiplier0: 0.0f32,
+                multiplier1: self.params.shavevapor_y5.value(),
+                sample_weighted: (d * 5.0f32),
+            },
+            d if d > 0.2f32 && d < 0.4f32 => ShaperSet {
+                multiplier0: self.params.shavevapor_y5.value(),
+                multiplier1: self.params.shavevapor_y6.value(),
+                sample_weighted: (d - 0.2f32 * 5.0f32),
+            },
+            d if d > 0.4f32 && d < 0.6f32 => ShaperSet {
+                multiplier0: self.params.shavevapor_y6.value(),
+                multiplier1: self.params.shavevapor_y7.value(),
+                sample_weighted: (d - 0.4f32 * 5.0f32),
+            },
+            d if d > 0.6f32 && d < 0.8f32 => ShaperSet {
+                multiplier0: self.params.shavevapor_y7.value(),
+                multiplier1: self.params.shavevapor_y8.value(),
+                sample_weighted: (d - 0.6f32 * 5.0f32),
+            },
+            d if d > 0.8f32 => ShaperSet {
+                multiplier0: self.params.shavevapor_y8.value(),
+                multiplier1: self.params.shavevapor_y9.value(),
+                sample_weighted: (d - 0.8f32 * 5.0f32),
+            },
+            _ => ShaperSet {
+                multiplier0: 1.0f32,
+                multiplier1: 1.0f32,
+                sample_weighted: sample,
+            },
+        };
+        s.multiplier0 * s.sample_weighted + ((1.0f32 - s.sample_weighted) * s.multiplier1)
     }
 }
 
 impl ClapPlugin for EvenPog {
     const CLAP_ID: &'static str = "volki9000.EvenPog";
-    const CLAP_DESCRIPTION: Option<&'static str> = Some("An EHX POG2 and the OG Eventide Pitch Shifter had a love child and it's been circuit-bent");
+    const CLAP_DESCRIPTION: Option<&'static str> = Some(
+        "An EHX POG2 and the OG Eventide Pitch Shifter had a love child and it's been circuit-bent",
+    );
     const CLAP_MANUAL_URL: Option<&'static str> = Some(Self::URL);
     const CLAP_SUPPORT_URL: Option<&'static str> = None;
     const CLAP_FEATURES: &'static [ClapFeature] = &[
@@ -276,7 +404,7 @@ impl ClapPlugin for EvenPog {
 
 impl Vst3Plugin for EvenPog {
     const VST3_CLASS_ID: [u8; 16] = *b"EvenPogPlugRvdH.";
-    const VST3_CATEGORIES: &'static str = "Fx|Filter";
+    const VST3_CATEGORIES: &'static str = "Fx|Distortion";
 }
 
 nih_export_clap!(EvenPog);
